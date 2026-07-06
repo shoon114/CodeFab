103c70e60d23bb392b280db4409d04a85534805c
Sanghoon Lee <shoon114@icloud.com>
2026-07-07 07:58:50 +0900

    [feature] VarDeclare 이항 표현식 초기화 커버리지 추가
    'var a = 3 + 4;'처럼 우변이 이항 표현식인 경우, exprParser가 반환하는
    BinaryExpr 모양 노드도 VarDeclareParser가 동일하게 attach함을 mock으로
    검증. 프로덕션 코드 변경 없음.

--- TestVarDeclareParser.cpp
+++ TestVarDeclareParser.cpp
@@ -119,4 +119,58 @@ TEST(VarDeclareParserTest, Parse_WithIdentifierInitializer_AttachesExpressionPar
 	EXPECT_THAT(valueNode->type, Eq(NodeType::Identifier));
 	EXPECT_THAT(valueNode->token.lexeme, Eq("b"));
 }
+
+TEST(VarDeclareParserTest, Parse_WithBinaryExprInitializer_AttachesExpressionParserResultAsValue) {
+	// var a = 3 + 4;  (VarDeclareParser must work the same regardless of the node shape exprParser returns)
+	TokenList tokenList = {
+		{ TokenType::KwVar, "var", 1, 1 },
+		{ TokenType::Identifier, "a", 1, 5 },
+		{ TokenType::Assign, "=", 1, 7 },
+		{ TokenType::Number, "3", 1, 9 },
+		{ TokenType::Plus, "+", 1, 11 },
+		{ TokenType::Number, "4", 1, 13 },
+		{ TokenType::Semicolon, ";", 1, 14 },
+		{ TokenType::EndOfFile, "", 1, 15 },
+	};
+
+	MockExpressionParser exprParser;
+	EXPECT_CALL(exprParser, Parse(_, _))
+		.WillOnce([](const TokenList& tokens, size_t& pos) {
+			auto left = std::make_unique<SyntaxNode>();
+			left->type = NodeType::NumberLiteral;
+			left->token = tokens[pos]; // '3'
+
+			const Token& opToken = tokens[pos + 1]; // '+'
+
+			auto right = std::make_unique<SyntaxNode>();
+			right->type = NodeType::NumberLiteral;
+			right->token = tokens[pos + 2]; // '4'
+
+			auto binaryNode = std::make_unique<SyntaxNode>();
+			binaryNode->type = NodeType::BinaryExpr;
+			binaryNode->token = opToken;
+			binaryNode->children.push_back(std::move(left));
+			binaryNode->children.push_back(std::move(right));
+
+			pos += 3; // consume '3', '+', '4'
+			return binaryNode;
+		});
+
+	VarDeclareParser parser(exprParser);
+	size_t pos = 0;
+	std::unique_ptr<SyntaxNode> root = parser.Parse(tokenList, pos);
+
+	ASSERT_THAT(root, NotNull());
+	ASSERT_THAT(root->children, SizeIs(1));
+
+	const std::unique_ptr<SyntaxNode>& assignNode = root->children[0];
+	ASSERT_THAT(assignNode->children, SizeIs(2));
+
+	const std::unique_ptr<SyntaxNode>& valueNode = assignNode->children[1];
+	EXPECT_THAT(valueNode->type, Eq(NodeType::BinaryExpr));
+	EXPECT_THAT(valueNode->token.lexeme, Eq("+"));
+	ASSERT_THAT(valueNode->children, SizeIs(2));
+	EXPECT_THAT(valueNode->children[0]->token.lexeme, Eq("3"));
+	EXPECT_THAT(valueNode->children[1]->token.lexeme, Eq("4"));
+}
 #endif
