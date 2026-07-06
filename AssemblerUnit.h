661752a7cdcab72e34ec615e1d62dd045ca0cf13
Sanghoon Lee <shoon114@icloud.com>
2026-07-07 08:42:26 +0900

    [feature] VarDeclare 구문 파싱 구현 (#8)
    'var a = 3;' 형태의 변수 선언/할당 토큰열을 VarDeclareStatement
    루트를 갖는 SyntaxTree로 구성하는 ParseVarDeclare()를 추가.
    현재는 고정된 토큰 순서만 지원하는 최소 구현.

--- AssemblerUnit.h
+++ AssemblerUnit.h
@@ -6,4 +6,7 @@
 class AssemblerUnit {
 public:
 	std::unique_ptr<SyntaxNode> Parse(const TokenList& tokenList);
+
+private:
+	std::unique_ptr<SyntaxNode> ParseVarDeclare(const TokenList& tokenList, size_t& pos);
 };
