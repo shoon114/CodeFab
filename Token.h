1dbc2f7ded39327852fb7ac953332e6268142bf5
J2Yoon <apfhd9043@naver.com>
2026-07-06 15:59:24 +0900

    [feature] token 및 tokenizer 클래스 구현 - token: token 자체 정보를 담는 struct - tokenizer: token 화 해주는 함수    -> 기존에 받은 코드문장들을 분리하고, 토큰 벡터를 만드는 역할

---
+++ Token.h
@@ -0,0 +1,54 @@
+#pragma once
+#include <string>
+#include <vector>
+
+enum class TokenType {
+    Number,
+	String,
+	Identifier,
+
+	KwVar,
+	KwIf,
+	KwElse,
+	KwWhile,
+	KwFunc,
+	KwReturn,
+	KwTrue,
+	KwFalse,
+
+	Plus,
+	Minus,
+	Star,
+	Slash,
+	Percent,
+	Assign,
+	Eq,
+	NotEq,
+	Lt,
+	Gt,
+	LtEq,
+	GtEq,
+	And,
+	Or,
+	Not,
+
+	LParen,
+	RParen,
+	LBrace,
+	RBrace,
+	Comma,
+	Semicolon,
+
+	EndOfFile
+};
+
+struct Token{
+    TokenType type;
+    std::string lexeme;
+    double realValue;
+    std::string originalCode;
+    int line;
+    int column;
+};
+
+using TokenList = std::vector<Token>;
