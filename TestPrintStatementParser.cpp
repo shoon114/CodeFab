63935ed1174a5c6a237ab56e4847541bb2de299a
Sangbeom <sangbum96@naver.com>
2026-07-07 15:21:36 +0900

    [refactoring]Test코드에 대한 중복코드 Fixture

--- TestPrintStatementParser.cpp
+++ TestPrintStatementParser.cpp
@@ -7,7 +7,59 @@
 
 using namespace testing;
 
-TEST(PrintStatementParserTest, Parse_PrintNumberLiteral_AttachesExpressionAsChild) {
+class PrintStatementParserTest : public Test {
+protected:
+	NiceMock<MockExpressionParser> exprParser;
+	PrintStatementParser parser{ exprParser };
+
+	void StubExprParserToConsume(int tokenCount, NodeType resultType) {
+		EXPECT_CALL(exprParser, Parse(_, _))
+			.Times(1)
+			.WillOnce([tokenCount, resultType](const TokenList& tokens, size_t& pos) {
+				auto node = std::make_unique<SyntaxNode>();
+				node->type = resultType;
+				node->token = tokens[pos];
+				pos += tokenCount;
+				return node;
+			});
+	}
+
+	void StubExprParserToThrow(const std::string& message) {
+		EXPECT_CALL(exprParser, Parse(_, _))
+			.Times(1)
+			.WillOnce([message](const TokenList&, size_t&) -> std::unique_ptr<SyntaxNode> {
+				throw std::runtime_error(message);
+			});
+	}
+
+	std::unique_ptr<SyntaxNode> ParseTokens(const TokenList& tokenList) {
+		size_t pos = 0;
+		return parser.Parse(tokenList, pos);
+	}
+
+	void ExpectParseThrows(const TokenList& tokenList, const char* expectedMessage) {
+		size_t pos = 0;
+		try {
+			parser.Parse(tokenList, pos);
+			FAIL();
+		}
+		catch (const std::runtime_error& e) {
+			EXPECT_STREQ(e.what(), expectedMessage);
+		}
+	}
+
+	void AssertPrintStmtWithExpression(const std::unique_ptr<SyntaxNode>& root, NodeType exprType) {
+		ASSERT_THAT(root, NotNull());
+		EXPECT_THAT(root->type, Eq(NodeType::PrintStmt));
+		EXPECT_THAT(root->token.lexeme, Eq("print"));
+		ASSERT_THAT(root->children, SizeIs(1));
+
+		const std::unique_ptr<SyntaxNode>& exprNode = root->children[0];
+		EXPECT_THAT(exprNode->type, Eq(exprType));
+	}
+};
+
+TEST_F(PrintStatementParserTest, Parse_PrintNumberLiteral_AttachesExpressionAsChild) {
 	// "print 3;"
 	TokenList tokenList = {
 		MakeToken(TokenType::Print, "print", 1, 0),
@@ -15,32 +67,14 @@ TEST(PrintStatementParserTest, Parse_PrintNumberLiteral_AttachesExpressionAsChil
 		MakeToken(TokenType::Semicolon, ";", 1, 2),
 		MakeToken(TokenType::EndOfFile, "", 1, 3),
 	};
+	StubExprParserToConsume(1, NodeType::NumberLiteral); // "3" 1개 토큰 소비
 
-	NiceMock<MockExpressionParser> exprParser;
-	EXPECT_CALL(exprParser, Parse(_, _))
-		.Times(1)
-		.WillOnce([](const TokenList& tokens, size_t& pos) {
-			auto node = std::make_unique<SyntaxNode>();
-			node->type = NodeType::NumberLiteral;
-			node->token = tokens[pos];
-			pos += 1; // "3" 1개 토큰 소비
-			return node;
-		});
+	std::unique_ptr<SyntaxNode> root = ParseTokens(tokenList);
 
-	PrintStatementParser parser{ exprParser };
-	size_t pos = 0;
-	std::unique_ptr<SyntaxNode> root = parser.Parse(tokenList, pos);
-
-	ASSERT_THAT(root, NotNull());
-	EXPECT_THAT(root->type, Eq(NodeType::PrintStmt));
-	EXPECT_THAT(root->token.lexeme, Eq("print"));
-	ASSERT_THAT(root->children, SizeIs(1));
-
-	const std::unique_ptr<SyntaxNode>& exprNode = root->children[0];
-	EXPECT_THAT(exprNode->type, Eq(NodeType::NumberLiteral));
+	AssertPrintStmtWithExpression(root, NodeType::NumberLiteral);
 }
 
-TEST(PrintStatementParserTest, Parse_PrintBinaryExpression_AttachesExpressionAsChild) {
+TEST_F(PrintStatementParserTest, Parse_PrintBinaryExpression_AttachesExpressionAsChild) {
 	// "print 2+3;"
 	TokenList tokenList = {
 		MakeToken(TokenType::Print, "print", 1, 0),
@@ -50,32 +84,14 @@ TEST(PrintStatementParserTest, Parse_PrintBinaryExpression_AttachesExpressionAsC
 		MakeToken(TokenType::Semicolon, ";", 1, 4),
 		MakeToken(TokenType::EndOfFile, "", 1, 5),
 	};
+	StubExprParserToConsume(3, NodeType::BinaryExpr); // "2+3" 3개 토큰 소비
 
-	NiceMock<MockExpressionParser> exprParser;
-	EXPECT_CALL(exprParser, Parse(_, _))
-		.Times(1)
-		.WillOnce([](const TokenList& tokens, size_t& pos) {
-			auto node = std::make_unique<SyntaxNode>();
-			node->type = NodeType::BinaryExpr;
-			node->token = tokens[pos];
-			pos += 3; // "2+3" 3개 토큰 소비
-			return node;
-		});
+	std::unique_ptr<SyntaxNode> root = ParseTokens(tokenList);
 
-	PrintStatementParser parser{ exprParser };
-	size_t pos = 0;
-	std::unique_ptr<SyntaxNode> root = parser.Parse(tokenList, pos);
-
-	ASSERT_THAT(root, NotNull());
-	EXPECT_THAT(root->type, Eq(NodeType::PrintStmt));
-	EXPECT_THAT(root->token.lexeme, Eq("print"));
-	ASSERT_THAT(root->children, SizeIs(1));
-
-	const std::unique_ptr<SyntaxNode>& exprNode = root->children[0];
-	EXPECT_THAT(exprNode->type, Eq(NodeType::BinaryExpr));
+	AssertPrintStmtWithExpression(root, NodeType::BinaryExpr);
 }
 
-TEST(PrintStatementParserTest, Parse_PrintDanglingOperator_ThrowsOnMalformedSyntax) {
+TEST_F(PrintStatementParserTest, Parse_PrintDanglingOperator_ThrowsOnMalformedSyntax) {
 	// "print a+;"   -- '+' 뒤 피연산자 누락
 	TokenList tokenList = {
 		MakeToken(TokenType::Print, "print", 1, 0),
@@ -84,23 +100,8 @@ TEST(PrintStatementParserTest, Parse_PrintDanglingOperator_ThrowsOnMalformedSynt
 		MakeToken(TokenType::Semicolon, ";", 1, 3),
 		MakeToken(TokenType::EndOfFile, "", 1, 4),
 	};
+	StubExprParserToThrow("Unexpected token while parsing expression");
 
-	NiceMock<MockExpressionParser> exprParser;
-	EXPECT_CALL(exprParser, Parse(_, _))
-		.Times(1)
-		.WillOnce([](const TokenList&, size_t&) -> std::unique_ptr<SyntaxNode> {
-			throw std::runtime_error("Unexpected token while parsing expression");
-		});
-
-	PrintStatementParser parser{ exprParser };
-	size_t pos = 0;
-
-	try {
-		parser.Parse(tokenList, pos);
-		FAIL();
-	}
-	catch (const std::runtime_error& e) {
-		EXPECT_STREQ(e.what(), "Unexpected token while parsing expression");
-	}
+	ExpectParseThrows(tokenList, "Unexpected token while parsing expression");
 }
 #endif
