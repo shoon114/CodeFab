5fc9872a5334a32489d51e43762c8ba91e326fe7
Sangbeom <sangbum96@naver.com>
2026-07-07 14:49:02 +0900

    [feature] Print 테스트 케이스 추가

--- TestPrintStatementParser.cpp
+++ TestPrintStatementParser.cpp
@@ -38,4 +38,39 @@ TEST(PrintStatementParserTest, Parse_PrintNumberLiteral_AttachesExpressionAsChil
 	const std::unique_ptr<SyntaxNode>& exprNode = root->children[0];
 	EXPECT_THAT(exprNode->type, Eq(NodeType::NumberLiteral));
 }
+
+TEST(PrintStatementParserTest, Parse_PrintBinaryExpression_AttachesExpressionAsChild) {
+	// "print 2+3;"
+	TokenList tokenList = {
+		MakeToken(TokenType::Print, "print", 1, 0),
+		MakeToken(TokenType::Number, "2", 1, 1),
+		MakeToken(TokenType::Plus, "+", 1, 2),
+		MakeToken(TokenType::Number, "3", 1, 3),
+		MakeToken(TokenType::Semicolon, ";", 1, 4),
+		MakeToken(TokenType::EndOfFile, "", 1, 5),
+	};
+
+	NiceMock<MockExpressionParser> exprParser;
+	EXPECT_CALL(exprParser, Parse(_, _))
+		.Times(1)
+		.WillOnce([](const TokenList& tokens, size_t& pos) {
+			auto node = std::make_unique<SyntaxNode>();
+			node->type = NodeType::BinaryExpr;
+			node->token = tokens[pos];
+			pos += 3; // "2+3" 3개 토큰 소비
+			return node;
+		});
+
+	PrintStatementParser parser{ exprParser };
+	size_t pos = 0;
+	std::unique_ptr<SyntaxNode> root = parser.Parse(tokenList, pos);
+
+	ASSERT_THAT(root, NotNull());
+	EXPECT_THAT(root->type, Eq(NodeType::PrintStmt));
+	EXPECT_THAT(root->token.lexeme, Eq("print"));
+	ASSERT_THAT(root->children, SizeIs(1));
+
+	const std::unique_ptr<SyntaxNode>& exprNode = root->children[0];
+	EXPECT_THAT(exprNode->type, Eq(NodeType::BinaryExpr));
+}
 #endif
