75d40a2bc83d4cf231e551afc4fb85117fc42fdf
Sanghoon Lee <shoon114@icloud.com>
2026-07-08 08:17:41 +0900

    [feature] Release 모드에서 콘솔 입력을 실행하는 system test용 main() 구현
    _DEBUG가 아닐 때 Tokenizer -> AssemblerUnit -> ExecutorUnit 파이프라인으로
    표준입력의 코드를 실행하도록 main()을 작성. print "hello"; 실행 결과를
    콘솔에서 확인 가능.

--- CodeFabInterpreter.cpp
+++ CodeFabInterpreter.cpp
@@ -1,6 +1,11 @@
 #ifdef _DEBUG
 #include "gmock/gmock.h"
 using namespace testing;
+#else
+#include <iostream>
+#include "Tokenizer.h"
+#include "AssemblerUnit.h"
+#include "ExecutorUnit.h"
 #endif
 
 int main() {
@@ -8,6 +13,20 @@ int main() {
 	InitGoogleMock();
 	return RUN_ALL_TESTS();
 #else
+	Tokenizer tokenizer;
+	tokenizer.GetCodeFromUser();
+	TokenList tokenList = tokenizer.CreateTokenForCode();
+
+	AssemblerUnit assembler;
+	ExecutorUnit executor;
+	try {
+		std::unique_ptr<SyntaxNode> tree = assembler.Parse(tokenList);
+		executor.Execute(*tree);
+	} catch (const std::exception& e) {
+		std::cerr << e.what() << std::endl;
+		return 1;
+	}
+
 	return 0;
 #endif
 }
