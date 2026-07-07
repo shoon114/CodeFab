36cfb0360f982b4837496cc2ff41a2d66d179981
Sangbeom Choi <81572235+choisang07@users.noreply.github.com>
2026-07-07 15:23:13 +0900

    Feature/assembler unit if (#29)
    * [feature]IF Statement에 대한 TEST 매크로 작성과 뼈대코드 작성
    
    * Revert "[refactoring]코드 머지 적용"
    
    This reverts commit c673aa31861ad219cf7fc9fb1a8f41228082bc54, reversing
    changes made to 1dd0d96c5d4eca9d4d349028f2fc16ab66d5794a.
    
    * Revert "[feature]IF Statement에 대한 TEST 매크로 작성과 뼈대코드 작성"
    
    This reverts commit 1dd0d96c5d4eca9d4d349028f2fc16ab66d5794a.
    
    * [refactoring] 코드 파싱 방식 일치화. 단어별로 인덱싱.
    
    * [feature] if문 GtEq, ERROR케이스에 대한
    
    * [feature]ERROR검증은 EXPECT_REQ
    
    * [refactoring]Nice Mocek
    
    * [refactoring]fixutre를 사용하여 Test코드 리팩토링, Parse에서 중복되는 에러구문도 refactoring진행
    
    * [refactoring]불필요한
    
    * [refactoring] 함수 Fixture로 옮기기.

--- IfStatementParser.cpp
+++ IfStatementParser.cpp
@@ -1,16 +1,26 @@
 #ifdef _DEBUG
+#include <stdexcept>
+#include <string>
 #include "IfStatementParser.h"
 #include "StatementParserRegistry.h"
 
 namespace {
-StatementParserRegistrar<IfStatementParser> registrar(TokenType::KwIf);
+	StatementParserRegistrar<IfStatementParser> registrar(TokenType::KwIf);
+
+	void ExpectToken(const TokenList& tokenList, size_t& pos, TokenType expected, const char* symbol) {
+		if (tokenList[pos].type != expected) {
+			throw std::runtime_error(std::string("Invalid Syntax. '") + symbol + "' is Missing");
+		}
+		pos++;
+	}
 }
 
 std::unique_ptr<SyntaxNode> IfStatementParser::Parse(const TokenList& tokenList, size_t& pos) {
 	const Token& ifToken = tokenList[pos++]; // 'if'
-	pos++; // '('
+
+	ExpectToken(tokenList, pos, TokenType::LParen, "(");
 	auto conditionNode = exprParser.Parse(tokenList, pos);
-	pos++; // ')'
+	ExpectToken(tokenList, pos, TokenType::RParen, ")");
 
 	auto ifNode = std::make_unique<SyntaxNode>();
 	ifNode->type = NodeType::IfStmt;
--- TestIfStatementParser.cpp
+++ TestIfStatementParser.cpp
@@ -1,4 +1,5 @@
 #ifdef _DEBUG
+#include <utility>
 #include "gmock/gmock.h"
 #include "IfStatementParser.h"
 #include "MockExpressionParser.h"
@@ -6,37 +7,62 @@
 
 using namespace testing;
 
+namespace {
+	TokenList MakeTokens(std::initializer_list<std::pair<TokenType, std::string>> tokens) {
+		TokenList result;
+		int column = 0;
+		for (const auto& [type, lexeme] : tokens) {
+			result.push_back(MakeToken(type, lexeme, 1, column++));
+		}
+		return result;
+	}
+}
+
 class IfStatementParserTest : public Test {
 protected:
-	MockExpressionParser exprParser;
+	NiceMock<MockExpressionParser> exprParser;
 	IfStatementParser parser{ exprParser };
 
-	// "if (a > 3)"
-	TokenList MakeIfConditionTokens() {
-		return TokenList{
-			MakeToken(TokenType::KwIf, "if", 1, 0),
-			MakeToken(TokenType::LParen, "(", 1, 1),
-			MakeToken(TokenType::Identifier, "a", 1, 2),
-			MakeToken(TokenType::Gt, ">", 1, 3),
-			MakeToken(TokenType::Number, "3", 1, 4),
-			MakeToken(TokenType::RParen, ")", 1, 5),
-			MakeToken(TokenType::EndOfFile, "", 1, 6),
-		};
+	// "if (a <op> 3)"
+	TokenList MakeIfConditionTokens(TokenType opType, const std::string& opLexeme) {
+		return MakeTokens({
+			{TokenType::KwIf, "if"},
+			{TokenType::LParen, "("},
+			{TokenType::Identifier, "a"},
+			{opType, opLexeme},
+			{TokenType::Number, "3"},
+			{TokenType::RParen, ")"},
+			{TokenType::EndOfFile, ""},
+			});
+	}
+
+	void StubExprParserToConsumeCondition() {
+		EXPECT_CALL(exprParser, Parse(_, _))
+			.Times(1)
+			.WillOnce([](const TokenList& tokens, size_t& pos) {
+			auto node = std::make_unique<SyntaxNode>();
+			node->type = NodeType::BinaryExpr;
+			node->token = tokens[pos];
+			pos += 3;
+			return node;
+				});
+	}
+
+	void ExpectParseThrows(TokenList tokenList, const char* expectedMessage) {
+		size_t pos = 0;
+		try {
+			parser.Parse(tokenList, pos);
+			FAIL();
+		}
+		catch (const std::runtime_error& e) {
+			EXPECT_STREQ(e.what(), expectedMessage);
+		}
 	}
 };
 
 TEST_F(IfStatementParserTest, Parse_Condition_AttachesExpressionParserResultAsCondition) {
-	TokenList tokenList = MakeIfConditionTokens();
-
-	EXPECT_CALL(exprParser, Parse(_, _))
-		.Times(1)
-		.WillOnce([](const TokenList& tokens, size_t& pos) {
-			auto node = std::make_unique<SyntaxNode>();
-			node->type = NodeType::BinaryExpr;
-			node->token = tokens[pos];
-			pos += 3; // consume "a > 3"
-			return node;
-		});
+	TokenList tokenList = MakeIfConditionTokens(TokenType::Gt, ">");
+	StubExprParserToConsumeCondition();
 
 	size_t pos = 0;
 	std::unique_ptr<SyntaxNode> root = parser.Parse(tokenList, pos);
@@ -48,5 +74,49 @@ TEST_F(IfStatementParserTest, Parse_Condition_AttachesExpressionParserResultAsCo
 
 	const std::unique_ptr<SyntaxNode>& conditionNode = root->children[0];
 	EXPECT_THAT(conditionNode->type, Eq(NodeType::BinaryExpr));
+}
+
+TEST_F(IfStatementParserTest, Parse_Condition_WithGtEqOperator_AttachesExpressionParserResultAsCondition) {
+	TokenList tokenList = MakeIfConditionTokens(TokenType::GtEq, ">=");
+	StubExprParserToConsumeCondition();
+
+	size_t pos = 0;
+	std::unique_ptr<SyntaxNode> root = parser.Parse(tokenList, pos);
+
+	ASSERT_THAT(root, NotNull());
+	EXPECT_THAT(root->type, Eq(NodeType::IfStmt));
+	EXPECT_THAT(root->token.lexeme, Eq("if"));
+	ASSERT_THAT(root->children, SizeIs(1));
+
+	const std::unique_ptr<SyntaxNode>& conditionNode = root->children[0];
+	EXPECT_THAT(conditionNode->type, Eq(NodeType::BinaryExpr));
+}
+
+TEST_F(IfStatementParserTest, Parse_MissingOpenParen_ThrowsOnMalformedSyntax) {
+	// if a>3)   -- '(' 누락
+	TokenList tokenList = MakeTokens({
+		{TokenType::KwIf, "if"},
+		{TokenType::Identifier, "a"},
+		{TokenType::Gt, ">"},
+		{TokenType::Number, "3"},
+		{TokenType::RParen, ")"},
+		{TokenType::EndOfFile, ""},
+		});
+
+	ExpectParseThrows(tokenList, "Invalid Syntax. '(' is Missing");
+}
+
+TEST_F(IfStatementParserTest, Parse_MissingCloseParen_ThrowsOnMalformedSyntax) {
+	// if(a>3   -- ')' 누락
+	TokenList tokenList = MakeTokens({
+		{TokenType::KwIf, "if"},
+		{TokenType::LParen, "("},
+		{TokenType::Identifier, "a"},
+		{TokenType::Gt, ">"},
+		{TokenType::Number, "3"},
+		{TokenType::EndOfFile, ""},
+		});
+
+	ExpectParseThrows(tokenList, "Invalid Syntax. ')' is Missing");
 }
 #endif
