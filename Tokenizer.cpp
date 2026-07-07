c83c4048f6fcbdddea2dd3e505ace68d4009d6ab
J2Yoon <apfhd9043@naver.com>
2026-07-07 13:15:47 +0900

    [Feature] GetCodeFromUser 함수 구현

--- Tokenizer.cpp
+++ Tokenizer.cpp
@@ -1,7 +1,12 @@
 #include "Tokenizer.h"
+#include <iostream>
+#include <sstream>
 
 void Tokenizer::GetCodeFromUser()
 {
+	std::ostringstream buffer;
+	buffer << std::cin.rdbuf();
+	originalCode = buffer.str();
 }
 
 std::vector<std::string> Tokenizer::SplitIntoWords()
