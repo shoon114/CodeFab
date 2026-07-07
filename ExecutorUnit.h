2a83ef92b35c0e202d899a08f943a782b97168a7
Sangbeom Choi <81572235+choisang07@users.noreply.github.com>
2026-07-07 15:31:01 +0900

    Master 브런치 Merge (#35)
    * [Feature]Test Checker Unit 추가 (#28)
    
    * Feature/assembler unit if (#29)
    
    * [feature]IF Statement에 대한 TEST 매크로 작성과 뼈대코드 작성
    
    * Revert "[refactoring]코드 머지 적용"
    
    This reverts commit c673aa31861ad219cf7fc9fb1a8f41228082bc54, reversing
    changes made to 1dd0d96c5d4eca9d4d349028f2fc16ab66d5794a.
    
    * Revert "[feature]IF Statement에 대한 TEST 매크로 작성과 뼈대코드 작성"
    
    This reverts commit 1dd0d96c5d4eca9d4d349028f2fc16ab66d5794a.
    
    * [refactoring] 코드 파싱 방식 일치화. 단어별로 인덱싱.
    
    * [feature] if문 GtEq, ERROR케이스에 대한
    
    * [feature]ERROR검증은 EXPECT_REQ
    
    * [refactoring]Nice Mocek
    
    * [refactoring]fixutre를 사용하여 Test코드 리팩토링, Parse에서 중복되는 에러구문도 refactoring진행
    
    * [refactoring]불필요한
    
    * [refactoring] 함수 Fixture로 옮기기.
    
    * [Feature] Implement ExecutorUnit additonal Expr (#32)
    
    * [feature] Add test case for ExcutorUnit.cpp
    
    * [feature] add exception for dividing zero value
    
    * [feature] add If Stmt test case
    
    * [refactoring] remove ternary Operator
    
    * [feature] Add ForStmt excutor test
    
    * [fix] modify VarDeclareStatement after master pull
    
    * [feature] add NestedIf test
    
    * [fix] rename TestExcutorUnit.cpp
    
    * [fix] rename TestExcutorUnit.cpp
    
    * [fix] remove endline in print stmt
    
    * [refactoring] use explict variable
    
    * [refactoring] remove unused enum value
    
    * [feature] implement local and global scope
    
    * [feature] Add Value_t with variant
    
    * [feature] add testcase for Executor Unit
    
    * [refactoring] modify condition check for bool and string
    
    * [refactoring] 함수 추상화
    
    * [feature] binaryExpr 미구현 기능 추가
    
    * [feature] implement UnaryExpr
    
    * [feature]Design Docs Update base on 26년 07월 07일 오후 2시 25분 (#30)
    
    ---------
    
    Co-authored-by: Daejin-Kim <daejinee.kim@gmail.com>
    Co-authored-by: joyjangs <56394629+joyjangs@users.noreply.github.com>

--- ExecutorUnit.h
+++ ExecutorUnit.h
@@ -1,7 +1,9 @@
-#pragma once
+﻿#pragma once
 #include "SyntaxNode.h"
+#include "Value.h"
 #include <string>
 #include <unordered_map>
+#include <vector>
 
 class ExecutorUnit {
 public:
@@ -9,7 +11,28 @@ public:
 
 private:
 	void ExecuteStmt(const SyntaxNode& node);
-	double Evaluate(const SyntaxNode& node);
+	Value_t Evaluate(const SyntaxNode& node);
 
-	std::unordered_map<std::string, double> variables;
+	// ExecuteStmt가 위임하는 문(statement)별 실행 메서드
+	void ExecutePrintStmt(const SyntaxNode& node);
+	void ExecuteVarDeclareStatement(const SyntaxNode& node);
+	void ExecuteBlockStmt(const SyntaxNode& node);
+	void ExecuteIfStmt(const SyntaxNode& node);
+	void ExecuteExprStmt(const SyntaxNode& node);
+	void ExecuteForStmt(const SyntaxNode& node);
+
+	// Evaluate가 위임하는 식(expression)별 평가 메서드
+	Value_t EvaluateIdentifier(const SyntaxNode& node);
+	Value_t EvaluateAssignExpr(const SyntaxNode& node);
+	Value_t EvaluateBinaryExpr(const SyntaxNode& node);
+	Value_t EvaluateUnaryExpr(const SyntaxNode& node);
+
+	void EnterScope();
+	void ExitScope();
+	Value_t& ResolveVariable(const std::string& name, int line);
+	double AsNumber(const Value_t& value, int line);
+	bool IsTruthy(const Value_t& value);
+
+	// scopes[0]이 Global 스코프, 이후 요소는 BlockStmt 진입마다 추가되는 Local 스코프.
+	std::vector<std::unordered_map<std::string, Value_t>> scopes;
 };
