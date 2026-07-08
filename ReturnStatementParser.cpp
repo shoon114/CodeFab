642a6ddfbef15d504aac18698d1e80c10b639e8b
Sangbeom <sangbum96@naver.com>
2026-07-08 14:04:03 +0900

    [feature]return이후 토큰 (

--- ReturnStatementParser.cpp
+++ ReturnStatementParser.cpp
@@ -8,14 +8,17 @@ StatementParserRegistrar<ReturnStatementParser> registrar(TokenType::KwReturn);
 std::unique_ptr<SyntaxNode> ReturnStatementParser::Parse(const TokenList& tokenList, size_t& pos) {
 	const Token& returnToken = tokenList[pos++];
 
-	std::shared_ptr<IStatementParser> exprParser = StatementParserRegistry::Instance().Resolve(tokenList[pos].type);
-	auto exprNode = exprParser->Parse(tokenList, pos);
-
-	pos++;
-
 	auto returnNode = std::make_unique<ReturnStmtNode>();
 	returnNode->token = returnToken;
-	returnNode->children.push_back(std::move(exprNode));
+
+	//return이후 토큰이 ;일 경우 바로 종료한다.
+	if (tokenList[pos].type != TokenType::Semicolon) {
+		std::shared_ptr<IStatementParser> exprParser = StatementParserRegistry::Instance().Resolve(tokenList[pos].type);
+		auto exprNode = exprParser->Parse(tokenList, pos);
+		returnNode->children.push_back(std::move(exprNode));
+	}
+
+	pos++;
 
 	return returnNode;
 }
