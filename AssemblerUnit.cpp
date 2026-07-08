cb63475437283ac457092fa87a0a154d0d60fa7f
Sanghoon Lee <shoon114@icloud.com>
2026-07-08 10:25:24 +0900

    [refactoring] AssemblerUnit::Parse의 while 조건을 HasMoreStatementsToParse로 분리
    pos < tokenList.size() && tokenList[pos].type != TokenType::EndOfFile 조건이
    루프 시그니처만 봐서는 의도(아직 파싱할 statement가 남았는지)를 바로
    파악하기 어려워, 이름이 있는 함수로 추출했다.

--- AssemblerUnit.cpp
+++ AssemblerUnit.cpp
@@ -2,11 +2,17 @@
 #include "StatementParserRegistry.h"
 #include <stdexcept>
 
+namespace {
+	bool HasMoreStatementsToParse(const TokenList& tokenList, size_t pos) {
+		return pos < tokenList.size() && tokenList[pos].type != TokenType::EndOfFile;
+	}
+}
+
 std::unique_ptr<SyntaxNode> AssemblerUnit::Parse(const TokenList& tokenList) {
 	auto programNode = std::make_unique<ProgramNode>();
 
 	size_t pos = 0;
-	while (pos < tokenList.size() && tokenList[pos].type != TokenType::EndOfFile) {
+	while (HasMoreStatementsToParse(tokenList, pos)) {
 		std::shared_ptr<IStatementParser> parser = StatementParserRegistry::Instance().Resolve(tokenList[pos].type);
 		if (parser == nullptr) {
 			throw std::runtime_error("Unexpected token at line " + std::to_string(tokenList[pos].line));
