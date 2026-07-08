ecc8bf68201a261a265ed7ce3bf9046f2982ad52
seunghoon <lsh107601@naver.com>
2026-07-08 13:07:12 +0900

    [feature] Add static array
    Add static array
    Add func expr

--- ExpressionParser.h
+++ ExpressionParser.h
@@ -14,7 +14,14 @@ private:
 
 	// Prefix position: unary operators, literals, identifiers, parenthesized expressions, calls.
 	std::unique_ptr<SyntaxNode> ParsePrefix(const TokenList& tokenList, size_t& pos);
+
+	// ParseAtom이 만든 식에 이어지는 postfix(함수 호출 '(...)', 인덱싱 '[...]')를
+	// 계속 붙여나간다 (예: arr[0], foo(1, 2), arr[i][j]).
 	std::unique_ptr<SyntaxNode> ParsePrimary(const TokenList& tokenList, size_t& pos);
+	std::unique_ptr<SyntaxNode> ParseAtom(const TokenList& tokenList, size_t& pos);
+	std::unique_ptr<SyntaxNode> ParseCallExpr(const TokenList& tokenList, size_t& pos, std::unique_ptr<SyntaxNode> callee);
+	std::unique_ptr<SyntaxNode> ParseArrExpr(const TokenList& tokenList, size_t& pos, Token calleeToken);
+	std::unique_ptr<SyntaxNode> ParseIndexExpr(const TokenList& tokenList, size_t& pos, std::unique_ptr<SyntaxNode> array);
 
 	static std::unique_ptr<SyntaxNode> MakeBinary(NodeType type, Token op, std::unique_ptr<SyntaxNode> left, std::unique_ptr<SyntaxNode> right);
 };
