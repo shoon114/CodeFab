a808ececf17d500c25d05129a51ba6516c96f753
Sangbeom <sangbum96@naver.com>
2026-07-10 08:57:54 +0900

    [feature]Number Watch

--- WatchList.cpp
+++ WatchList.cpp
@@ -18,11 +18,11 @@ bool WatchList::Contains(const std::string& name) const {
 }
 
 void WatchList::PrintAll(const ExecutorUnit& executor) const {
-	// TODO(watch 담당자): names를 순회하며 executor.TryGetVariable로 값을 조회해 출력
 	for (const std::string& name : names) {
 		Value_t value;
 		if (executor.TryGetVariable(name, value)) {
-			std::cout << name << " = " << std::get<double>(value) << std::endl;
+			bool isLocal = executor.CurrentScope().count(name) > 0;
+			std::cout << (isLocal ? "[LOCAL] " : "[GLOBAL] ") << name << " = " << std::get<double>(value) << " (number)" << std::endl;
 		} else {
 			std::cout << name << " = <undefined>" << std::endl;
 		}
