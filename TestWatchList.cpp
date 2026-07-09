224db7fd56fb846b29007259cf642a883b43e343
Sangbeom <sangbum96@naver.com>
2026-07-10 08:51:53 +0900

    [feature]Test케이스 watches

--- TestWatchList.cpp
+++ TestWatchList.cpp
@@ -19,9 +19,52 @@ TEST(WatchListTest, Add_NewName_IsInList) {
 TEST(WatchListTest, Remove_WatchedName_IsNoLongerInList) {
 	WatchList watchList;
 	watchList.Add("a");
-	
+
 	watchList.Remove("a");//unWatch
 
 	EXPECT_FALSE(watchList.Contains("a"));
+}
+
+// var a = 4; 실행 후(전역 스코프에 남아있는 상태) watches를 조회하면
+// "[LOCAL] a = 4 (number)"가 출력되어야 한다.
+TEST(WatchListTest, PrintAll_GlobalNumberVariable_PrintsGlobalLabelAndNumberType) {
+	auto identifier = std::make_unique<IdentifierNode>();
+	identifier->token.type = TokenType::Identifier;
+	identifier->token.lexeme = "a";
+	identifier->token.line = 1;
+
+	auto numberLiteral = std::make_unique<NumberLiteralNode>();
+	numberLiteral->token.type = TokenType::Number;
+	numberLiteral->token.realValue = 4;
+	numberLiteral->token.line = 1;
+
+	auto assign = std::make_unique<AssignExprNode>();
+	assign->token.type = TokenType::Assign;
+	assign->token.lexeme = "=";
+	assign->token.line = 1;
+	assign->children.push_back(std::move(identifier));
+	assign->children.push_back(std::move(numberLiteral));
+
+	auto varDecl = std::make_unique<VarDeclareStatementNode>();
+	varDecl->token.type = TokenType::KwVar;
+	varDecl->token.lexeme = "var";
+	varDecl->token.line = 1;
+	varDecl->children.push_back(std::move(assign));
+
+	auto program = std::make_unique<ProgramNode>();
+	program->children.push_back(std::move(varDecl));
+
+	ExecutorUnit executor;
+	executor.Execute(*program);
+
+	//watch a
+	WatchList watchList;
+	watchList.Add("a");
+
+	testing::internal::CaptureStdout();
+	watchList.PrintAll(executor);
+	std::string output = testing::internal::GetCapturedStdout();
+
+	EXPECT_THAT(output, HasSubstr("[LOCAL] a = 4 (number)"));
 }
 #endif
